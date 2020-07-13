26e69a76f7be98a9bab48b827ef1b636c6c5b9c6
i386sh <24914325+i386sh@users.noreply.github.com>
2020-07-13 21:32:16 +1000

    A whole bunch of features, like commands, nick changes, fix of user bios, ignoring users, room changes.

--- app/index.js
+++ app/index.js
@@ -32,6 +32,18 @@ io.on('connection', async (socket) => {
         onlineUsers
     });
 
+    socket.on('disconnect', (data) => {
+        console.log(`[DEBUG]: Socket ${socket.id} disconnected.`);
+        if (!userData[socket.id]) return console.log(`[DEBUG]: Socket ${socket.id} had no userData.`);
+        io.to(userData[socket.id].currentRoom).emit("user_update", {
+            user: userData[socket.id],
+            type: "leave"
+        });
+        delete userData[socket.id];
+        if (messageDelay.has(socket.id)) messageDelay.delete(socket.id);
+        onlineUsers -= 1;
+    });
+
     socket.on('chat_message', (data) => {
         console.log(`[DEBUG]: Socket ${socket.id} sent a chat_message packet.`);
         if (messageDelay.has(socket.id)) {
@@ -42,43 +54,91 @@ io.on('connection', async (socket) => {
         data["userData"] = userData[socket.id];
         if (data["content"].length > 250) return console.log(`[DEBUG]: Socket ${socket.id} tried to send a message with more than 250 characters.`);
         data["content"] = xss(data["content"], config.xssFilter);
-        
-        messageDelay.add(socket.id);
-        let sendToRoom = data["room"] || "general";
 
-        io.to(sendToRoom.toLowerCase()).emit("chat_message", data);
+        messageDelay.add(socket.id);
+
+        io.to(userData[socket.id].currentRoom).emit("chat_message", data);
         setTimeout(() => {
             messageDelay.delete(socket.id);
         }, 250);
     });
 
+    socket.on('user_command', (data) => {
+        console.log(`[DEBUG]: Socket ${socket.id} sent a user_command packet.`);
+        let command = data.command || "";
+        switch (command) {
+            case "listOnlineUsers":
+                 console.log(`[DEBUG]: Socket ${socket.id} wanted to know the online users.`);
+                var res = [];
+                for (var i in userData)
+                    res.push(userData[i].nickName + ` (${userData[i].userID})`);
+                socket.emit("command_output", {
+                    text: `All currently online users: ${res.join(", ")}.`
+                });
+            break;
+            case "listAllRooms":
+                console.log(`[DEBUG]: Socket ${socket.id} wanted to know the current rooms.`);
+
+                socket.emit("command_output", {
+                    text: `Rooms: ${rooms.join(", ")}.`
+                });
+            break;
+            default:
+                socket.emit("command_output", {
+                    text: `Is someone tinkering in the console?`
+                });
+            break;
+        }
+    });
+
     socket.on("userData_change", (data) => {
         console.log(`[DEBUG]: Socket ${socket.id} sent a userData_change packet.`);
 
-        switch(data.type) {
+        switch (data.type) {
 
-            case "nickname":
+            case "nickName":
+                console.log(`[DEBUG]: Socket ${socket.id} is changing their nickname.`)
                 let oldName = userData[socket.id]["nickName"];
                 userData[socket.id]["nickName"] = xss(data.newNick.replace(/"/g, `\\"`), config.xssFilter);
-                io.to("general").emit("user_update", {
+                io.to(userData[socket.id].currentRoom).emit("user_update", {
                     type: "nickChange",
                     oldName,
                     newName: xss(data.newNick, config.xssFilter),
                     user: userData[socket.id]
                 });
-            break;
+                break;
 
             case "bio":
-                if(!data.bio || data.bio.length > 125) return console.log(`[DEBUG]: Socket ${socket.id} tried to update their bio with a null value or a bio with more than 125 characters.`);    
+                console.log(`[DEBUG]: Socket ${socket.id} is updating their bio.`)
+                if (!data.bio || data.bio.length > 125) return console.log(`[DEBUG]: Socket ${socket.id} tried to update their bio with a null value or a bio with more than 125 characters.`);
                 userData[socket.id]["bio"] = xss(data.bio.replace(/"/g, `\'`).replace(/'/g, `\\'`), config.xssFilter);
                 socket.emit("command_output", {
                     text: "Bio updated."
-                })
-            break;
+                });
+                break;
+
+            case "room":
+                console.log(`[DEBUG]: Socket ${socket.id} is moving rooms.`)
+                if (!rooms.includes(data.newRoom)) rooms.push(data.newRoom);
+                let oldRoom = userData[socket.id].currentRoom;
+                socket.leave(oldRoom);
+                io.to(oldRoom).emit("user_update", {
+                    type: "leaveRoom",
+                    oldRoom,
+                    user: userData[socket.id]
+                });
+                socket.join(data.newRoom);
+                io.to(data.newRoom).emit("user_update", {
+                    type: "joinRoom",
+                    newRoom: data.newRoom,
+                    user: userData[socket.id]
+                });
+                userData[socket.id]["currentRoom"] = data.newRoom;
+                break;
 
             default:
-            console.log(`[DEBUG]: Socket ${socket.id} sent an unknown userData_change type.`)
-            break;
+                console.log(`[DEBUG]: Socket ${socket.id} sent an unknown userData_change type.`)
+                break;
 
         }
 
@@ -93,10 +153,11 @@ io.on('connection', async (socket) => {
             "nickName": xss(data["nickName"], config.xssFilter),
             "colour": `${Math.floor(Math.random() * 16777216).toString(16)}`,
             "bio": xss(data["bio"].replace(/"/g, `\\"`).replace(/'/g, `\\'`), config.xssFilter),
-            "userID": Math.floor(Math.random() * 9000000000) + 1000000000
+            "userID": Math.floor(Math.random() * 9000000000) + 1000000000,
+            "currentRoom": "general"
         };
 
-        io.to("general").emit("user_update", {
+        io.to(userData[socket.id].currentRoom).emit("user_update", {
             user: userData[socket.id],
             type: "join"
         });
@@ -104,4 +165,4 @@ io.on('connection', async (socket) => {
     });
 });
 
-http.listen(3000);
+http.listen(config.port);
--- app/web/chat.js
+++ app/web/chat.js
@@ -8,11 +8,17 @@ let clientData = {
     connected: true
 }
 
+function arrayRemove(arr, value) {
+    return arr.filter(function (ele) {
+      return ele != value;
+    });
+}
+
 function append(data) {
     $("#messages").append($(`<li>`).text(data));
 }
 
-const socket = io();
+const socket = io({reconnect: false});
 
 socket.on("user_connection", () => {
     if(clientData.connected == false) return;
@@ -23,7 +29,12 @@ socket.on("user_connection", () => {
     socket.emit("userData_init", userData);
 });
 
+socket.on('disconnect', (data)=> {
+clientData.connected = false;
+});
+
 socket.on('chat_message', (data) => {
+    if(clientData.ignored.includes(`${data.userData.userID}`)) return;
     let onclick_text = `onclick="append('ID: ${data.userData.userID} - ${data.userData.bio}')"`
     $("#messages").append($(`<li><span ${onclick_text} style="color:#${data.userData.colour}">${data.userData.nickName}</span> >> ${data.content}</li>`));
 });
@@ -35,13 +46,19 @@ socket.on('command_output', (data) => {
 socket.on('user_update', (data) => {
     switch(data.type) {
         case "join":
-            $("#messages").append($(`<li><span style="color:#${data.user.colour}">${data.user.nickName}</span> joined.</li>`));
+            $("#messages").append($(`<li><span style="color:#${data.user.colour}">${data.user.nickName}</span> joined the chatbox.</li>`));
         break;
         case "leave":
-            $("#messages").append($(`<li><span style="color:#${data.user.colour}">${data.user.nickName}</span> left.</li>`));
+            $("#messages").append($(`<li><span style="color:#${data.user.colour}">${data.user.nickName}</span> left the chatbox.</li>`));
         break;
         case "nickChange":
             $("#messages").append($(`<li><span style="color:#${data.user.colour}">${data.oldName}</span> is now <span style="color:#${data.user.colour}">${data.newName}</span>.</li>`));
+        break;
+        case "joinRoom":
+            $("#messages").append($(`<li><span style="color:#${data.user.colour}">${data.user.nickName}</span> joined the room.</li>`));
+        break;
+        case "leaveRoom":
+            $("#messages").append($(`<li><span style="color:#${data.user.colour}">${data.user.nickName}</span> left the room.</li>`));
         break;
         default:
             console.log("Receieved unknown user_update.");
@@ -52,6 +69,83 @@ socket.on('user_update', (data) => {
 $('form').submit((event) => {
     event.preventDefault();
     let message = $("#m").val();
+
+    if(message.startsWith(`//`)) {
+    const args = message.slice("//").trim().split(/ +/g);
+    const command = args.shift().toLowerCase();
+
+    switch(command) {
+        case "//nick":
+            if(!args[0]) return append("Usage: //nick <nickname>");
+            if(args[0].trim() == "") return append("Usage: //nick <nickname>");
+
+            socket.emit("userData_change", {
+                type: "nickName",
+                newNick: args.join(" ")
+            });
+        break;
+        case "//bio":
+            if(!args[0]) return append("Usage: //bio <text: char max 125>");
+            if(args[0].trim() == "") return append("Usage: //bio <text: char max 125>");
+            if(args.join(" ").length > 125) return append("Usage: //bio <text: char max 125>");
+
+            socket.emit("userData_change", {
+                type: "bio",
+                bio: args.join(" ")
+            });
+        break;
+        case "//ignore":
+            if(!args[0]) return append("Usage: //ignore <user ID>");
+            if(args[0].trim() == "") return append("Usage: //ignore <user ID>");
+
+            if(clientData.ignored.includes(`${args[0]}`)) {
+                clientData.ignored = arrayRemove(clientData.ignored, args[0]);
+                append(`Removed ${args[0]} from the ignore list.`);
+            } else {
+                clientData.ignored.push(`${args[0]}`);
+                append(`Added ${args[0]} to the ignore list.`);
+            }
+        break;
+        case "//room":
+            if(!args[0]) return append("Usage: //room change,list,current <room name>");
+            if(args[0].trim() == "") return append("Usage: //room change,list,current <room name>");
+            
+            switch(args[0].toLowerCase()) {
+                case "change":
+                    if(!args[1]) return append("Usage: //room change <room name>");
+                    if(args[1].trim() == "") return append("Usage: //room change <room name>");
+
+                    socket.emit("userData_change", {
+                        type: "room",
+                        newRoom: args[1]
+                    });
+                    clientData["room"] = args[1];
+
+                break;
+                case "current":
+                    append(`You are currently in ${clientData.room}.`);
+                break;
+                case "list":
+                    socket.emit("user_command", {
+                        command: "listAllRooms"
+                    });
+                break;
+            }
+        break;
+        case "//online":
+            socket.emit("user_command", {
+                command: "listOnlineUsers"
+            });
+        break;
+        case "//help":
+            append(`Available commands: //online, //room, //nick, //bio, //ignore.`);
+        break;
+    }
+
+    $("#m").val("");
+    return;
+    }
+
     socket.emit("chat_message", {
         content: message,
         userData, 
